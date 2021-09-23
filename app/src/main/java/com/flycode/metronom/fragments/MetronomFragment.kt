a7860c2d8628ef797173079e717241a6c3809be3
DrugCowBoy <88621143+DrugCowBoy@users.noreply.github.com>
2021-09-23 17:08:25 +0300

    Архитектура на Navigation Component и Moxy

--- app/src/main/java/com/flycode/metronom/fragments/MetronomFragment.kt
+++ app/src/main/java/com/flycode/metronom/fragments/MetronomFragment.kt
@@ -1,14 +1,28 @@
 package com.flycode.metronom.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
+import com.flycode.metronom.MainActivity
 import com.flycode.metronom.R
 import moxy.MvpAppCompatFragment
 
 class MetronomFragment: MvpAppCompatFragment(R.layout.fragment_metronom) {
 
+    lateinit var btnUp: ImageView
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        btnUp = view.findViewById(R.id.imageBtnUp)
+
+        btnUp.setOnClickListener {
+            Log.i("My", "btn")
+            Toast.makeText(requireContext(), "Up", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
