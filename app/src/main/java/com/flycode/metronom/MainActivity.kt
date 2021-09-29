492936c665debacdd0f5fdc0dc0b8f7c6a013778
Vadim <monctrik@yandex.ru>
2021-09-29 17:50:37 +0300

    макет

--- app/src/main/java/com/flycode/metronom/MainActivity.kt
+++ app/src/main/java/com/flycode/metronom/MainActivity.kt
@@ -4,10 +4,9 @@ import android.app.ProgressDialog.show
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Layout
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.TextView
+import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -16,6 +15,10 @@ import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
+    var linearLayoutSize: LinearLayout? = null
+    var linearLayoutDrawing: LinearLayout? = null
+
+
     lateinit var imageButtonSize: ImageButton
     lateinit var imageButtonDrawing: ImageButton
 
@@ -23,23 +26,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        imageButtonSize = findViewById(R.id.imageButtonSize)
 
+
+
+
+        imageButtonSize = findViewById(R.id.imageButtonSize)
         imageButtonSize?.setOnClickListener {
             supportFragmentManager.let {
                 SizeBottomSheetDialog.newInstance(Bundle()).apply {
                     show(it, tag)
                 }
+                linearLayoutSize = findViewById(R.id.linearLayoutSize)
+                val layoutInflater = LayoutInflater.from(this)
+                val view: View = layoutInflater.inflate(R.layout.size, linearLayoutSize, false)
+                linearLayoutSize?.addView(view)
             }
         }
 
         imageButtonDrawing = findViewById(R.id.imageButtonDrawing)
-
         imageButtonDrawing?.setOnClickListener {
             supportFragmentManager.let {
                 DrawingBottomSheetDialog.newDInstance(Bundle()).apply {
                     show(it, tag)
                 }
+                linearLayoutDrawing = findViewById(R.id.linearLayoutDrawing)
+                val layoutInflater = LayoutInflater.from(this)
+                val view: View = layoutInflater.inflate(R.layout.drawing, linearLayoutDrawing, false)
+                linearLayoutDrawing?.addView(view)
             }
         }
 
