0d6ca5cb8300cc8a5a81863ac6575c3cb7202742
Vadim <monctrik@yandex.ru>
2021-10-04 15:46:09 +0300

    макет

--- app/src/main/java/com/flycode/metronom/DrawingBottomSheetDialog.kt
+++ app/src/main/java/com/flycode/metronom/DrawingBottomSheetDialog.kt
@@ -1,19 +1,52 @@
 package com.flycode.metronom
 
+import android.content.Intent
+import android.media.Image
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.ScrollView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 //всплывающий экран с нотами
 class DrawingBottomSheetDialog : BottomSheetDialogFragment(){
 
+   var scrollView3: ScrollView? = null
+    var imageViewDrawingNotes: ImageView? = null
+    var imageView: ImageView? = null
+    var imageView2: ImageView? = null
+    var imageView3: ImageView? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.drawing, container, false)
+        imageViewDrawingNotes = view?.findViewById(R.id.imageViewDrawingNotes)
+        scrollView3 = view?.findViewById(R.id.scrollView3)
+        imageView = view?.findViewById(R.id.imageView)
+        imageView2 = view?.findViewById(R.id.imageView2)
+        imageView3 = view?.findViewById(R.id.imageView3)
+
+
+        imageView?.setOnClickListener {
+            imageViewDrawingNotes!!.setImageResource(R.drawable.note1)
+            this.startActivity(Intent(requireContext(), MainActivity::class.java))
+        }
+
+        imageView2?.setOnClickListener {
+            imageViewDrawingNotes!!.setImageResource(R.drawable.note2)
+            this.startActivity(Intent(requireContext(), MainActivity::class.java))
+        }
+
+        imageView3?.setOnClickListener {
+            imageViewDrawingNotes!!.setImageResource(R.drawable.note3)
+            this.startActivity(Intent(requireContext(), MainActivity::class.java))
+        }
     }
 
     companion object {
+
         @JvmStatic
         fun newDInstance(bundle: Bundle): DrawingBottomSheetDialog {
             val fragment = DrawingBottomSheetDialog()
--- app/src/main/java/com/flycode/metronom/MainActivity.kt
+++ app/src/main/java/com/flycode/metronom/MainActivity.kt
@@ -2,6 +2,7 @@ package com.flycode.metronom
 
 import android.app.ProgressDialog.show
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,11 +13,13 @@ import android.view.ViewTreeObserver
 import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.graphics.drawable.toDrawable
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import org.w3c.dom.Text
 import java.util.*
 import kotlin.properties.Delegates
+import kotlin.random.Random.Default.nextInt
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,24 +29,20 @@ class MainActivity : AppCompatActivity() {
     var linearLayoutDrawing: LinearLayout? = null
     var imageButtonSize: ImageButton? = null
     var imageButtonDrawing: ImageButton? = null
-    var scrollView2: ScrollView? = null
-    var txt_2: TextView? = null
-    var txt_3: TextView? = null
-    var txt_4: TextView? = null
     var imageButtonSetting: ImageButton? = null
-    var txt_double4: TextView? = null
     var imageButtonFraction4: ImageButton? = null
     var imageButtonFraction1: ImageButton? = null
     var imageButtonFraction2: ImageButton? = null
     var imageButtonFraction3: ImageButton? = null
-    var dd: Array<Image>? = null
-
+var flag: Boolean? = null
+var flag2: Boolean? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textViewFractionSizeMain = findViewById(R.id.textViewFractionSize)
-        var ss: Boolean? = null
+        flag = true
+        flag2 = false
 
         //вызов всплывающего экрана с размером
         imageButtonSize = findViewById(R.id.imageButtonSize)
@@ -94,82 +93,64 @@ class MainActivity : AppCompatActivity() {
         imageButtonFraction2 = findViewById(R.id.imageButtonFraction2)
         imageButtonFraction3 = findViewById(R.id.imageButtonFraction3)
 
+        imageButtonFraction1?.setOnClickListener {
+
+            if (flag!!) {
+                imageButtonFraction1!!.setImageResource(R.drawable.rec1)
+                flag = false
+                flag2 = false
+            }else if (flag2!!){
+                imageButtonFraction1!!.setImageResource(R.drawable.rec3)
+                flag = true }
+            else{
+                imageButtonFraction1!!.setImageResource(R.drawable.rec2)
+                flag2 = true
+            }
+        }
 
 
 
+        imageButtonFraction2?.setOnClickListener {
+            if (flag!!) {
+                imageButtonFraction2!!.setImageResource(R.drawable.rec1)
+                flag = false
+                flag2 = false
+            }else if (flag2!!){
+                imageButtonFraction2!!.setImageResource(R.drawable.rec3)
+                flag = true }
+            else{
+                imageButtonFraction2!!.setImageResource(R.drawable.rec2)
+                flag2 = true
+            }
+        }
 
+        imageButtonFraction3?.setOnClickListener {
+            if (flag!!) {
+                imageButtonFraction3!!.setImageResource(R.drawable.rec1)
+                flag = false
+                flag2 = false
+            }else if (flag2!!){
+                imageButtonFraction3!!.setImageResource(R.drawable.rec3)
+                flag = true }
+            else{
+                imageButtonFraction3!!.setImageResource(R.drawable.rec2)
+                flag2 = true
+            }
+        }
 
-
-        /* imageButtonFraction1?.setOnClickListener {
-             if (flag){
-                 imageButtonFraction1!!.setImageResource(R.drawable.rectangle_sv)
-                 flag = false
-             }else{
-                 imageButtonFraction1!!.setImageResource(R.drawable.rectangle)
-                 flag = true
-             }
-         }
-
-         imageButtonFraction2?.setOnClickListener {
-             if (flag){
-                 imageButtonFraction2!!.setImageResource(R.drawable.rectangle_sv)
-                 flag = false
-             }else{
-                 imageButtonFraction2!!.setImageResource(R.drawable.rectangle)
-                 flag = true
-             }
-         }
-
-         imageButtonFraction3?.setOnClickListener {
-             if (flag){
-                 imageButtonFraction3!!.setImageResource(R.drawable.rectangle_sv)
-                 flag = false
-             }else{
-                 imageButtonFraction3!!.setImageResource(R.drawable.rectangle)
-                 flag = true
-             }
-         }
-
-         imageButtonFraction20?.setOnClickListener {
-             if (flag){
-                 imageButtonFraction20!!.setImageResource(R.drawable.rectangle_sv)
-                 flag = false
-             }else{
-                 imageButtonFraction20!!.setImageResource(R.drawable.rectangle)
-                 flag = true
-             }
-         }
-
-         imageButtonFraction21?.setOnClickListener {
-             if (flag){
-                 imageButtonFraction21!!.setImageResource(R.drawable.rectangle_sv)
-                 flag = false
-             }else{
-                 imageButtonFraction21!!.setImageResource(R.drawable.rectangle)
-                 flag = true
-             }
-         }
-
-         imageButtonFraction22?.setOnClickListener {
-             if (flag){
-                 imageButtonFraction22!!.setImageResource(R.drawable.rectangle_sv)
-                 flag = false
-             }else{
-                 imageButtonFraction22!!.setImageResource(R.drawable.rectangle)
-                 flag = true
-             }
-         }
-
-         imageButtonFraction23?.setOnClickListener {
-             if (flag){
-                 imageButtonFraction23!!.setImageResource(R.drawable.rectangle_sv)
-                 flag = false
-             }else{
-                 imageButtonFraction23!!.setImageResource(R.drawable.rectangle)
-                 flag = true
-             }
-         }*/
-
+        imageButtonFraction4?.setOnClickListener {
+            if (flag!!) {
+                imageButtonFraction4!!.setImageResource(R.drawable.rec1)
+                flag = false
+                flag2 = false
+            } else if (flag2!!) {
+                imageButtonFraction4!!.setImageResource(R.drawable.rec3)
+                flag = true
+            } else {
+                imageButtonFraction4!!.setImageResource(R.drawable.rec2)
+                flag2 = true
+            }
+        }
     }
 
 
